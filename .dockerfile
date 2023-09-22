f4f975d2304226bb0185b8e3eebce9785422c263
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-09-22 17:50:26 -0500

    Dockerfiles

---
+++ .dockerfile
@@ -0,0 +1,15 @@
+FROM node:latest
+
+WORKDIR /app
+
+COPY package.json package-lock.json ./
+COPY tsconfig.json ./
+COPY src ./src
+
+RUN npm install
+
+RUN npm run compile
+
+EXPOSE 3000
+
+CMD ["node", "dist/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
  res.send('Hello World!');
