1cd57d1e67d3adaeeeb5d6e4d40b191564702cd5
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-09-26 19:01:36 -0500

    Boilerplate and watch mode as startWatch

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 // src/app.ts
 import express from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(helmet());
 
 app.get('/', (req, res) => {
- res.send('Hello World!');
+  res.send('Hello World!');
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
