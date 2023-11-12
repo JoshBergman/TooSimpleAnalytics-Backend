92545d9b38047e956fd6c3032bb528a1b37583d9
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-11-12 16:42:49 -0600

    Dummy data now uses a normal distribution in an attempt to make dummy data seem more realistic

--- src/helpers/make-dummy-data.ts
+++ src/helpers/make-dummy-data.ts
@@ -1,6 +1,21 @@
 import { IUser } from "../interfaces/user";
 
 export const makeDummyData = () => {
+  const generateNormalDistribution = (mean: number, stdDev: number): number => {
+    let u1 = 0;
+    let u2 = 0;
+
+    // Generate two independent random variables with a uniform distribution (0, 1)
+    while (u1 === 0) u1 = Math.random();
+    while (u2 === 0) u2 = Math.random();
+
+    // Apply the Box-Muller transform to generate random numbers with a standard normal distribution
+    const z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(2.0 * Math.PI * u2);
+
+    // Scale and shift the random number to the desired mean and standard deviation
+    return mean + stdDev * z0;
+  };
+
   let totalViews = 0;
   const two_years_of_random_view_dates = () => {
     const currentDate = new Date();
@@ -18,7 +33,11 @@ export const makeDummyData = () => {
 
         for (let day = 1; day <= totalDaysInMonth; day++) {
           // Generate a random number of views (for example, between 1 and 100)
-          const randomViews = 10 + Math.floor(Math.random() * 4) + 1;
+          const meanViews = 40;
+          const stdDevViews = 2;
+          const randomViews = Math.round(
+            generateNormalDistribution(meanViews, stdDevViews)
+          );
 
           totalViews += randomViews;
 
